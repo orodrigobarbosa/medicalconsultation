f595ea3db408194c42ad04dbea68b5ac53ee1a69
Rodrigo Barbosa <rodbarbosa.dv@gmail.com>
2024-09-06 12:44:03 -0300

    Crud Simples

--- src/main/java/com/medicalconsultation/demo/usuario/resources/UsuarioController.java
+++ src/main/java/com/medicalconsultation/demo/usuario/resources/UsuarioController.java
@@ -17,11 +17,35 @@ public class UsuarioController {
     private UsuarioService usuarioService;
 
     @PostMapping
-    public ResponseEntity<Usuario> cadastrarUsuario(@RequestBody Usuario usuario){
+    public ResponseEntity<Usuario> cadastrarUsuario(@RequestBody Usuario usuario) {
         Usuario novoUsuario = usuarioService.cadastrarUsuario(usuario);
         return ResponseEntity.status(HttpStatus.CREATED).body(novoUsuario);
     }
 
+    @GetMapping
+    public ResponseEntity<List<Usuario>> buscarTodosUsuarios() {
+        List<Usuario> usuarios = usuarioService.listarUsuarios();
+        return ResponseEntity.ok().body(usuarios);
+    }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Usuario> buscarUsuarioPorId(@PathVariable Long id) {
+        Usuario usuario = usuarioService.buscarUsuarioPorId(id);
+        return ResponseEntity.ok().body(usuario);
+    }
 
+    @PutMapping(value = "atualizar/{id}")
+    public ResponseEntity<Usuario> atualizarUsuarioViaId(@RequestBody Usuario usuario, @PathVariable Long id) {
+        Usuario usuarioAtualizado = usuarioService.atualizarUsuarioPorId(usuario, id);
+        return ResponseEntity.ok().body(usuarioAtualizado);
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public ResponseEntity<Void> deletarUsuarioPorId(@PathVariable Long id) {
+        usuarioService.deletarUsuarioPorId(id);
+        return ResponseEntity.noContent().build();
+    }
 }
+
+
+
