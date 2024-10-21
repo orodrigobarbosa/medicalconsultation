2c749d48c47d87eb373a57a3541f4a424ba7ad4f
rodrigo barbosa <rodbarbosa.dev@gmail.com>
2024-10-21 20:35:33 -0300

    introducao a testes

--- src/test/java/com/medicalconsultation/demo/usuario/services/UsuarioServiceTest.java
+++ src/test/java/com/medicalconsultation/demo/usuario/services/UsuarioServiceTest.java
@@ -9,13 +9,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.postgresql.hostchooser.HostRequirement.any;
 
 @ExtendWith(MockitoExtension.class)
-
 class UsuarioServiceTest {
 
     @InjectMocks
@@ -27,22 +29,53 @@ class UsuarioServiceTest {
 
     //teste de cadastrar usuario
     @Test
-    void  cadastrarUsuario(){
-     Usuario usuario = new Usuario();
-     usuario.setNomeUsuario("Rodrigo");
+    void cadastrarUsuario() {
+        Usuario usuario = new Usuario();
+        usuario.setNomeUsuario("Rodrigo");
 
-     //obedece a seguinte regra:
-    // configuração do comportamento do MOCK
-     when(usuarioRepository.save(any(Usuario.class))).thenReturn(usuario);
+        //obedece a seguinte regra:
+        // configuração do comportamento do MOCK
+        when(usuarioRepository.save(any(Usuario.class))).thenReturn(usuario);
 
 
-    //A execução do método a ser testado
-    var resaltado = usuarioService.cadastrarUsuario(usuario);
+        //A execução do método a ser testado
+        var resultado = usuarioService.cadastrarUsuario(usuario);
 
-    //Validação do Teste
+        //Validação do Teste
         assertNotNull(usuario);
-        assertEquals("Barbosa", resaltado.getNomeUsuario());
+        assertEquals("Barbosa", resultado.getNomeUsuario());
 
         verify(usuarioRepository, times(1)).save(usuario);
 
-}}
+    }
+
+    @Test
+    void listarUsuarios(){
+
+        Usuario usuario1 = new Usuario();
+        usuario1.setNomeUsuario("Maria");
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setNomeUsuario("João");
+
+        List<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(usuario1);
+        usuarios.add(usuario2);
+
+        //config do comportamento do MOCK
+
+        when(usuarioRepository.findAll()).thenReturn(usuarios);
+
+        //A execucao do metodo a ser testado
+       var resultado = usuarioService.listarUsuarios();
+
+        //Validacao do Teste
+
+        assertNotNull(resultado);
+        assertEquals(2,resultado.size());
+        assertEquals("Maria", resultado.get(0).getNomeUsuario());
+        assertEquals("Joãoo", resultado.get(1).getNomeUsuario(), "O nome do usuario 2 está incorreto");
+    }
+
+
+}
